220fd005141aa4222eb43da4fc1309329a076511
Pehel Bagrecha <pehel.bagrecha@gmail.com>
2026-08-17 17:23:18 +0530

    Test workflow failure

--- math_test.go
+++ math_test.go
@@ -6,7 +6,7 @@ func TestAdd(t *testing.T) {
 
     result := Add(5, 3)
 
-    expected := 8
+    expected := 10
 
     if result != expected {
         t.Errorf("Expected %d but got %d", expected, result)
